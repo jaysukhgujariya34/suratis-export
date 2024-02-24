be36e765d4e87b868267dae0ef49cbd74be68794
jaysukh22 <125646448+jaysukh22@users.noreply.github.com>
2024-02-24 23:49:14 +0530

    first commit

---
+++ src/mock/data/authData.js
@@ -0,0 +1,9 @@
+export const signInUserData = [
+  {
+    id: "1",
+    avatar: "/img/avatars/thumb-1.jpg",
+    userName: "admin",
+    email: "suratiexport@gmail.com",
+    password: "admin@123",
+  },
+];
